5e25e9e07f086b5c9172d47027a79a16e61b9592
San1tater <98987733+San1tater1122@users.noreply.github.com>
2025-11-26 11:32:34 -0500

    Add basic user input and arithmetic operations

---
+++ Nov26, 2025/PYCLASS1.py
@@ -0,0 +1,10 @@
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+print("Your name is", name, "and you are", age, "years old.")
+
+numA = int(input("Please print the 1st number: "))
+numB = int(input("Please print the 2nd number: "))
+print("The sum of the two number is", numA+numB, ", the difference is", numA-numB, ", the product is", numA*numB, ".")
+
+degreeC = int(input("Please print the degree in C: "))
+print(degreeC, "Celsius is", ((degreeC * 9/5) + 32), "degree Fahrenheit.")
